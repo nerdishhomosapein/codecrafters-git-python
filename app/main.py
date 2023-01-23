e6a116eda921416f841a468146de655962492ce5
Kumar Prateek <kumarprateek902@gmail.com>
2023-01-23 23:44:55 +0530

    Stage-2

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 import sys
 import os
+import zlib
 
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
+    # print("Logs from your program will appear here!")
 
     # Uncomment this block to pass the first stage
     #
@@ -16,6 +17,13 @@ def main():
         with open(".git/HEAD", "w") as f:
             f.write("ref: refs/heads/master\n")
         print("Initialized git directory")
+    elif command == "cat-file":
+        blob_hash = sys.argv[3]
+        with open(f".git/objects/{blob_hash[:2]}/{blob_hash[2:]}", "rb") as f:
+            decompressed = zlib.decompress(f.read())
+            decompressed = decompressed.decode("utf-8")
+            decompressed = decompressed.split("\x00")
+            print(decompressed[1], end="")
     else:
         raise RuntimeError(f"Unknown command #{command}")
 
